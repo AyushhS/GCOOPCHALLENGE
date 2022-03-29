81245177863c3e9471c1457fa308b2bed855d9ab
Ayush Soni <83175870+AyushhS@users.noreply.github.com>
2022-03-29 12:53:49 +0530

    formation of classes

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Gymkhana {};
+class President {};
+class Vice_president {};
+class General_secretary {};
+class Secretary {};
+class Student {};
+class Society {};
+class Tech: public Society {};
+class Sports: public Society {};
+class Cult: public Society {};
+
+int main() {
+
+}
